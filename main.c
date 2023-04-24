0945a7d88a1553fa12946d7eaaccb428d1a00446
bdotsamir <samir.js.zip@gmail.com>
2023-04-24 19:17:39 -0400

    main: get author name

--- main.c
+++ main.c
@@ -1,6 +1,30 @@
+// REFERENCES:
+// [1]: https://www.tutorialspoint.com/cprogramming/c_file_io.htm
+// [2]: https://www.geeksforgeeks.org/c-program-to-read-contents-of-whole-file/#
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main() {
-    printf("Hello, World!\n");
+    FILE *federalist = fopen("./assets/Federalist1.txt", "r");
+    if(federalist == NULL) {
+        fprintf(stderr, "No such file\n");
+        exit(1);
+    }
+
+    char BUF[256];
+
+    char authorFirstName[20], authorLastName[20];
+    while(fscanf(federalist, "%s", BUF) == 1) {
+        if(strncmp(BUF, "Author", 6) == 0) {
+            fscanf(federalist, "%s %s", authorFirstName, authorLastName);
+            printf("Author: %s %s\n", authorFirstName, authorLastName);
+            break;
+        }
+//        printf("%s ", BUF);
+    }
+
+    fclose(federalist);
     return 0;
 }
