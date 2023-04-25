75d9b7ccb115fdd0cca1227beccb68d4930b1462
bdotsamir <samir.js.zip@gmail.com>
2023-04-25 16:50:31 -0400

    util/array: create resizable array

---
+++ util/array.c
@@ -0,0 +1,53 @@
+// REFERENCES:
+// [1]: https://clang.llvm.org/extra/clang-tidy/checks/bugprone/suspicious-realloc-usage.html
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "array.h"
+
+// Defined in the header
+//typedef struct {
+//    int *array;
+//    size_t used;
+//    size_t size;
+//} Array;
+
+void initArray(Array *a, const size_t initialSize) {
+    a->array = calloc(initialSize, sizeof(int));
+    if(!a->array) {
+        fprintf(stderr, "Failed to allocate memory of size %lu\n", initialSize);
+        exit(EXIT_FAILURE);
+    }
+
+    a->used = 0;
+    a->size = initialSize;
+}
+
+void push(Array *a, int data) {
+    // If the array has already reached its maximum size,
+    if(a->size == a->used) {
+        // Set the new size to itself times 2.
+        // This could be optimized, but we have virtually unlimited memory, so it isn't too bad.
+        // It's not like we're embedded programmers lul
+        // See other: https://stackoverflow.com/a/1100426/8916706
+
+        // Actually reallocate that block of memory to the new size
+        int *p = reallocarray(a->array, a->size *= 2, sizeof(int));
+        // If the reallocation fails, exit.
+        if(!p) {
+            fprintf(stderr, "Failed to reallocate array of new size %lu\n", a->size);
+            exit(EXIT_FAILURE);
+        }
+        a->array = p; // REF: [1]
+        fprintf(stderr, "[DEBUG] Reallocated array with new size %lu\n", a->size);
+    }
+
+    a->array[a->used++] = data;
+
+}
+
+void freeArray(Array *a) {
+    free(a->array);
+    a->size = 0;
+    a->used = 0;
+}
---
+++ util/array.h
@@ -0,0 +1,36 @@
+//
+// Created by samir on 4/25/23.
+//
+
+#ifndef FEDERALIST_PAPERS_ARRAY_H
+#define FEDERALIST_PAPERS_ARRAY_H
+
+#endif //FEDERALIST_PAPERS_ARRAY_H
+
+#include <stdlib.h>
+
+typedef struct {
+    int *array;
+    size_t used;
+    size_t size;
+} Array;
+
+/**
+ * Initialize a new, empty array
+ * @param a Array structure pointer
+ * @param initialSize Initial size of the array
+ */
+void initArray(Array *a, const size_t initialSize);
+
+/**
+ * Push a new element onto the array, automatically reallocating if necessary
+ * @param a Array structure pointer
+ * @param data Integer data to push
+ */
+void push(Array *a, int data);
+
+/**
+ * Free the memory used by the array
+ * @param a Array structure pointer
+ */
+void freeArray(Array *a);
