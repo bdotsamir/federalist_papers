d19083047ddf6d8e4bd3fbb9c88e7396e1b817df
bdotsamir <samir.js.zip@gmail.com>
2023-04-25 18:07:59 -0400

    create test/index.js: test js file so I can check my C output against my JS output

---
+++ test/index.js
@@ -0,0 +1,5 @@
+// This test file is in a language I am most familiar with.
+
+// The only reason why this file exists is that I need to make sure that the C code I am writing
+// has the correct outputs. If I program it in JavaScript, I can be 100% confident that the C code
+// I write is also correct if they have the same output.
