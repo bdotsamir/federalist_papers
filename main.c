4d0336fc48c537e0448a078e6731b05d6fd32f9d
bdotsamir <samir.js.zip@gmail.com>
2023-04-25 18:07:32 -0400

    main: implement average function, implement endsWith function, create main.h, calculate average word/sentence length

--- main.c
+++ main.c
@@ -1,30 +1,73 @@
 // REFERENCES:
 // [1]: https://www.tutorialspoint.com/cprogramming/c_file_io.htm
 // [2]: https://www.geeksforgeeks.org/c-program-to-read-contents-of-whole-file/#
+// [3]: https://www.geeksforgeeks.org/how-to-input-or-read-a-character-word-and-a-sentence-from-user-in-c/#
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "main.h"
 
-int main() {
+int main(void) {
     FILE *federalist = fopen("./assets/Federalist1.txt", "r");
     if(federalist == NULL) {
         fprintf(stderr, "No such file\n");
         exit(1);
     }
 
-    char BUF[256];
-
-    char authorFirstName[20], authorLastName[20];
-    while(fscanf(federalist, "%s", BUF) == 1) {
-        if(strncmp(BUF, "Author", 6) == 0) {
-            fscanf(federalist, "%s %s", authorFirstName, authorLastName);
-            printf("Author: %s %s\n", authorFirstName, authorLastName);
-            break;
-        }
-//        printf("%s ", BUF);
+    FILE *result = fopen("./result.bin", "ab");
+    if(result == NULL) {
+        fprintf(stderr, "Failed to open result file\n");
+        exit(1);
     }
+
+    char BUF[256];
+    Array wordLengthArray;
+    initArray(&wordLengthArray, 8);
+
+    Array sentenceLengthArray;
+    initArray(&sentenceLengthArray, 8);
+
+    Author author;
+
+    int currentSentenceWordCount = 0;
+    while(fscanf(federalist, "%s", BUF) == 1) {
+//        printf("%i BUF %s \n", currentSentenceWordCount, BUF);
+        // If we come across the author declaration in this Federalist paper, set the corresponding variables.
+        if(strncmp(BUF, "Author", strlen("Author")) == 0) {
+            fscanf(federalist, "%s %s", author.firstname, author.lastname);
+            printf("Author: %s %s\n", author.firstname, author.lastname);
+        }
+
+        if(endsWith(BUF, '.', strlen(BUF)) == 1) {
+//            fprintf(stderr, "[DEBUG] New sentence! Previous sentence length: %i words\n\n", currentSentenceWordCount);
+            push(&sentenceLengthArray, currentSentenceWordCount);
+            currentSentenceWordCount = 0;
+        }
+
+        int wordLength = (int) strlen(BUF);
+        push(&wordLengthArray, wordLength);
+        currentSentenceWordCount++;
+    }
+
+    printf("\nAverage word length: %f characters\n", average(&wordLengthArray));
+    printf("Average sentence word length: %f words\n", average(&sentenceLengthArray));
+
+    freeArray(&wordLengthArray);
 
     fclose(federalist);
     return 0;
+}
+
+float average(Array *a) {
+    int sum = 0;
+    for(int i = 0; i < a->used; i++) {
+        sum += a->array[i];
+    }
+    float avg = (float) sum / (float) a->used;
+    return avg;
+}
+
+int endsWith(const char *src, char c, size_t n) {
+    return src[n - 1] == c ? 1 : 0;
 }
---
+++ main.h
@@ -0,0 +1,38 @@
+//
+// Created by samir on 4/25/23.
+//
+
+#ifndef FEDERALIST_PAPERS_MAIN_H
+#define FEDERALIST_PAPERS_MAIN_H
+
+#endif //FEDERALIST_PAPERS_MAIN_H
+
+#include "util/array.h"
+
+typedef struct {
+    char firstname[20];
+    char lastname[20];
+} Author;
+
+typedef struct {
+    Author author;
+    float avgWordLength;
+    float avgSentenceLength;
+    float avgFillerWords;
+} FederalistPaper;
+
+/**
+ * Find the average of a given Array struct
+ * @param a Pointer to an Array struct
+ * @return The average
+ */
+float average(Array *a);
+
+/**
+ * Find if a given src string of n ends with c
+ * @param src Source string
+ * @param c Character to test
+ * @param n Length of string
+ * @return 1 = true; 0 = false
+ */
+int endsWith(const char *src, char c, size_t n);
